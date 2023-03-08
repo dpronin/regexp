87cd8e660c3211824599d14c1d9f8bb424ce6b27
Denis Pronin <dannftk@yandex.ru>
2023-03-08 11:46:15 +0300

    make 'regexp' binary work line grep digesting from STDIN
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- regexp.cpp
+++ regexp.cpp
@@ -1,10 +1,22 @@
 #include <cerrno>
+#include <cstdint>
+#include <cstring>
+
+#include <iostream>
 
 #include "regexplib.hpp"
 
 int main(int argc, char const* argv[])
 {
-    if (argc < 3)
+    if (argc < 2) {
+        std::cerr << "no pattern given\n";
         return EINVAL;
-    return regexp::does_match(argv[1], argv[2]) ? 0 : 1;
+    }
+
+    for (std::string line; std::cin >> line;) {
+        if (regexp::does_match(line, argv[1]))
+            std::cout << line << std::endl;
+    }
+
+    return 0;
 }
